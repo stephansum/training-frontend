785b0867e9a8c565c0ec005ad2074e137f2adb50
Stephan Sum <sum@live.de>
2016-08-10 00:03:31 +0200

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('./**/*.scss')
-        .pipe(sass())
+    gulp.src('./playground_modules/**/*.scss') // to watch ALL: ./**/*.scss
+        .pipe(sass().on('error', sass.logError))  // .on('error', sass.logError)  to swallow error
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
 });
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('./**/*.scss', ['sass']);
+    gulp.watch('./playground_modules/**/*.scss', ['sass']);
 })
