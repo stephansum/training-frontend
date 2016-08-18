30d784158d71471b5c5794dd80a365cc2720728a
Stephan Sum <sum@live.de>
2016-08-18 09:55:06 +0200

    improved gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('./playground_modules/**/*.scss') // to watch ALL: ./**/*.scss
-        .pipe(sass().on('error', sass.logError))  // .on('error', sass.logError)  to swallow error
+    gulp.src('playground_modules/**/*.scss') // to watch ALL: ./**/*.scss
+        .pipe(sass().on('error', sass.logError)) // .on('error', sass.logError)  to swallow error
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
 });
 
+
+
 gulp.task('default', ['sass'], function() {
-    gulp.watch('./playground_modules/**/*.scss', ['sass']);
+    // dont use "./" in front of pathes, because otherwise gulp-watch isnt able to recognize newly or deleted files
+    gulp.watch('playground_modules/**/*.scss', ['sass']);
 })
