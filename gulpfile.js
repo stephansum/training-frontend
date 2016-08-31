3856531d342d6a188a74422859e707dc16db5d18
Stephan Sum <sum@live.de>
2016-08-31 19:39:22 +0200

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 // Sass configuration
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var sass = require('gulp-sass');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
 
 gulp.task('sass', function() {
     gulp.src('playground_modules/**/*.scss') // to watch ALL: ./**/*.scss
@@ -11,8 +15,18 @@ gulp.task('sass', function() {
 });
 
 
+gulp.task('bundlejs', function() {
+    browserify('main.js') // start analyzing main.js for further required files
+        .bundle()
+        .on('error', function(e) {
+            gutil.log(e);
+        })
+        .pipe(source('bundle.js')) // create vinyl stream which is required by gulp
+        .pipe(gulp.dest('./dist/js')) // folder where the bundle.js will be created
+});
+
 
 gulp.task('default', ['sass'], function() {
     // dont use "./" in front of pathes, because otherwise gulp-watch isnt able to recognize newly or deleted files
     gulp.watch('playground_modules/**/*.scss', ['sass']);
-})
+});
---
+++ typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/browserify/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/gulp/index.d.ts" />
