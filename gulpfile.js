00ad3ac7d1c1d8adff80307e1c942d4d97902b35
Stephan Sum <sum@live.de>
2016-09-21 22:52:18 +0200

    restructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,36 @@
 // Sass configuration
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var includer = require('gulp-file-include');
+
 var sass = require('gulp-sass');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
 
 gulp.task('sass', function() {
-    gulp.src('playground_modules/**/*.scss') // to watch ALL: ./**/*.scss
+    return gulp
+        .src('playground_modules/**/*.scss') // to watch ALL: ./**/*.scss
         .pipe(sass().on('error', sass.logError)) // .on('error', sass.logError)  to swallow error
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
+});
+
+
+
+
+gulp.task('includer', function() {
+    return gulp.src('./src/**/*.html')
+        .pipe(plumber())
+        .pipe(includer({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest(function(f) {
+            return f.base;
+        }));
 });
 
 
@@ -26,7 +45,22 @@ gulp.task('bundlejs', function() {
 });
 
 
-gulp.task('default', ['sass'], function() {
+
+gulp.task('watch', function() {
     // dont use "./" in front of pathes, because otherwise gulp-watch isnt able to recognize newly or deleted files
     gulp.watch('playground_modules/**/*.scss', ['sass']);
+
+    gulp.watch('path/to/file', ['gulp task name for js']);
+});
+
+
+gulp.task('js', function() {
+    return gulp
+        .src('./src/**/*.js')
+});
+
+
+
+gulp.task('default', ['sass'], function() {
+
 });
